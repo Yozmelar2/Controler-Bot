2fcf386680a1178de85cf84967be34a7030efc86
Yozmelar2 <93841516+Yozmelar2@users.noreply.github.com>
2022-10-15 09:28:23 +0500

    Update Config.py

--- Config.py
+++ Config.py
@@ -17,11 +17,11 @@ if ENVIRONMENT:
         MUST_JOIN = MUST_JOIN.replace("@", "")
 else:
     # Fill the Values
-    API_ID = 0
-    API_HASH = ""
-    BOT_TOKEN = ""
-    DATABASE_URL = ""
+    API_ID = 534493
+    API_HASH = "ac922823455e814e44020a9f3ee17914"
+    BOT_TOKEN = "5752725687:AAHkApA42hWm1wA_JAa94H4ddlDrRisAI0Q"
+    DATABASE_URL = "mongodb+srv://my_bot:toshqul96@cluster0.djaqdiv.mongodb.net/?retryWrites=true&w=majority"
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
-    MUST_JOIN = ""
+    MUST_JOIN = "hacker_oken"
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN[1:]
